ee0c9b142fe2245e5cc1d441a0012963e218a44b
aryaandra <aryaandra44@gmail.com>
2024-09-06 15:36:07 +0700

    komet

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('login');
-});
-
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
